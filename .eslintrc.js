7aa653c822ebb70d6f1a909e9c3652b48c626c5d
maroof-ahmed <mav_rock@hotmail.com>
2021-07-27 20:04:56 +0500

    feature/vs-init: Added App Antd Layout and Tailwind CSS

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': ['error', { singleQuote: true }],
+    'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
   },
   overrides: [
     {
