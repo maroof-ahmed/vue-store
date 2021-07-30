51d307ddeead607dfe544c72639e7597551fda3f
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-07-30 19:10:18 +0500

    render product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" class="w-full mb-5" />
-    <a-button type="primary" @click="incrementStateCount">
-      Increment State Count
-    </a-button>
-    <a-button type="default" @click="updateStateCount">
-      Update State Count
-    </a-button>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Products />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// TODO: Lazy Load Components
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import { Button } from 'ant-design-vue';
-
-Vue.use(Button);
+import Products from '@/views/Products.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Products,
   },
 })
-export default class Home extends Vue {
-  incrementStateCount(): void {
-    this.$store.dispatch('incrementCountAction');
-  }
-  updateStateCount(): void {
-    this.$store.dispatch('updateCountAction');
-  }
-}
+export default class Home extends Vue {}
 </script>
+
+<style scoped></style>
